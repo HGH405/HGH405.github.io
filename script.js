cfd245f91f920cc372973a475f6043adeef42bb0
HGH405 <48737929+HGH405@users.noreply.github.com>
2019-03-20 08:08:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-// Plain Javascript Example
-var $jsName = document.querySelector('.name');
-var $jsValue = document.querySelector('.jsValue');
 
-$jsName.addEventListener('input', function(event){
-  $jsValue.innerHTML = $jsName.value;
-}, false);
 
 
 // JQuery example
